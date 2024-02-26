38d776e2915140a699a1c60f2015ae99f46d9d05
DefinitelyNotSimon13 <simon21.blum@gmail.com>
2024-02-26 16:35:18 +0100

    Update main.cpp for easylogging

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
+#include <easylogging++.h>
 #include <iostream>
 
-/** \todo 
+/** \todo
  * - Github
  * - "Dev-Ops"
  * - Doxygen settings
  * - Template-Comment
  * - Template-Header-Comment
- * 
+ *
  **/
+
+INITIALIZE_EASYLOGGINGPP
+
+namespace WIP {
+void exampleEasyLogging();
+}
 
 /**
  * \brief Main function
@@ -17,6 +24,37 @@
  * - Formatter: astyle
  **/
 int main(int argc, char *argv[]) {
+  WIP::exampleEasyLogging();
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
+
+/**
+ * \brief Namespace for work in progress
+ *
+ * \details
+ * - This namespace is for code that is not finished yet
+ * - It is used to keep the main namespace clean
+ **/
+namespace WIP {
+/**
+ * \brief Example of how to use easylogging with a configuration file
+ *
+ * \details
+ * - This function is an example of how to use easylogging
+ * - The configuration file is located in ../conf
+ * - Before proper integration, config has to be done properly
+ *
+ * \todo 
+ * - Configure easylogging properly
+ * - outsource easylogging config
+ *  - e.g. startup class?
+ **/
+void exampleEasyLogging() {
+  el::Configurations conf("conf/easylogging.conf");
+  el::Loggers::reconfigureLogger("default", conf);
+  el::Loggers::reconfigureAllLoggers(conf);
+
+  LOG(INFO) << "My first info log using default logger";
+}
+} // namespace WIP

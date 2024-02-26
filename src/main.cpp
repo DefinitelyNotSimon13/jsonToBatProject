d7441f1820093037769a64a0b9293c7974196725
DefinitelyNotSimon13 <simon21.blum@gmail.com>
2024-02-26 09:48:52 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-int main (int argc, char *argv[]) {
+/**
+ * @brief Main function
+ * @details
+ * TODO:
+ * - Github
+ * - "Dev-Ops"
+ * - Doxygen settings
+ *
+ * Codeconvention:
+ * - Formatter: astyle
+ **/
+int main(int argc, char *argv[]) {
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }

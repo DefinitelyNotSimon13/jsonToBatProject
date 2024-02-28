0820e4d44ef6a8a84bfb66dbe3b5f6214920211c
DefinitelyNotSimon13 <simon21.blum@gmail.com>
2024-02-28 08:48:39 +0100

    Another try for fixing windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "easylogging++.h"
 
 //https://github.com/Chunde/getopt-for-windows
-#ifdef _WIN32
+#ifdef IS_WINDOWS
 #include <xgetopt.h>
 #else
 #include <getopt.h>
@@ -33,44 +33,45 @@ void exampleEasyLogging();
  * - Template-Comment
  * - Template-Header-Comment
  **/
-int main(int argc, char *argv[]) {
-/// \note Debug
+int main(int argc, char* argv[])
+{
+    /// \note Debug
 #ifdef IS_LINUX
-  std::cout << "Linux\n\n" << std::endl;
+    std::cout << "Linux\n\n" << std::endl;
 #elif IS_WINDOWS
-  std::cout << "Windows\n\n" << std::endl;
+    std::cout << "Windows\n\n" << std::endl;
 #else
-  std::cout << "Unknown OS\n\n" << std::endl;
+    std::cout << "Unknown OS\n\n" << std::endl;
 #endif
+    utils::StartupHandler::initEasyLogging();
 
-  utils::StartupHandler::initEasyLogging();
+    if (argc <= 1) {
+        LOG(WARNING) << "No arguments provided, exiting!";
+        std::cout << "No arguments provided, exiting!\n";
+        return 1;
+    }
 
-  if (argc <= 1) {
-    LOG(WARNING) << "No arguments provided, exiting!";
-    std::cout << "No arguments provided, exiting!\n";
-    return 1;
-  }
+    std::cout << "Hello, World!" << std::endl;
+    std::optional<std::string> filename;
 
-  std::cout << "Hello, World!" << std::endl;
-  std::optional<std::string> filename;
+    try {
+        filename = utils::StartupHandler::getOptions(argc, argv);
+    }
+    catch (const std::invalid_argument &e) {
+        LOG(WARNING) << "Caught invalid argument: " << e.what();
+        std::cout << "Invalid argument: " << e.what() << std::endl;
+    }
 
-  try {
-    filename = utils::StartupHandler::getOptions(argc, argv);
-  } catch (const std::invalid_argument &e) {
-    LOG(WARNING) << "Caught invalid argument: " << e.what();
-    std::cout << "Invalid argument: " << e.what() << std::endl;
-  }
+    if (!filename.has_value()) {
+        LOG(ERROR) << "No filename given! Exiting...";
+        std::cerr << "No filename given!\nExiting...\n";
+        return 1;
+    }
 
-  if (!filename.has_value()) {
-    LOG(ERROR) << "No filename given! Exiting...";
-    std::cerr << "No filename given!\nExiting...\n";
-    return 1;
-  }
-
-  LOG(INFO) << "Filename received: " << filename.value();
-  std::cout << "Filename: " << filename.value() << std::endl;
-  LOG(INFO) << "Further processing...";
-  std::cout << "Further processing..." << std::endl;
-  LOG(INFO) << "Application exiting!";
-  return 0;
+    LOG(INFO) << "Filename received: " << filename.value();
+    std::cout << "Filename: " << filename.value() << std::endl;
+    LOG(INFO) << "Further processing...";
+    std::cout << "Further processing..." << std::endl;
+    LOG(INFO) << "Application exiting!";
+    return 0;
 }
--- src/sources/StartupHandler.cpp
+++ src/sources/StartupHandler.cpp
@@ -1,7 +1,7 @@
 #include "StartupHandler.hpp"
 #include "easylogging++.h"
 
-#ifdef _WIN32
+#ifdef IS_WINDOWS
 #include <xgetopt.h>
 #else
 #include <getopt.h>
@@ -15,76 +15,81 @@ namespace utils {
 
 static int verbose = 0;
 
-void StartupHandler::initEasyLogging() {
-  el::Configurations conf("conf/easylogging.conf");
-  el::Loggers::reconfigureLogger("default", conf);
-  el::Loggers::reconfigureAllLoggers(conf);
-  LOG(INFO) << "Easylogging initialized!";
+void StartupHandler::initEasyLogging()
+{
+    el::Configurations conf("conf/easylogging.conf");
+    el::Loggers::reconfigureLogger("default", conf);
+    el::Loggers::reconfigureAllLoggers(conf);
+    LOG(INFO) << "Easylogging initialized!";
 }
 
-std::optional<std::string> StartupHandler::getOptions(int argc, char *argv[]) {
-  LOG(INFO) << "Parsing options...";
-  static const struct option long_options[] = {
-      /* These options set a flag. */
-      {"verbose", no_argument, &verbose, 1},
-      {"brief", no_argument, &verbose, 0},
-      {"help", no_argument, nullptr, 'h'},
-      {"version", no_argument, nullptr, 'V'},
-      {"test", required_argument, nullptr, 0},
-      nullptr};
+std::optional<std::string> StartupHandler::getOptions(int argc, char* argv[])
+{
+    LOG(INFO) << "Parsing options...";
+    static const struct option long_options[] = {
+        /* These options set a flag. */
+        {"verbose", no_argument, &verbose, 1},
+        {"brief", no_argument, &verbose, 0},
+        {"help", no_argument, nullptr, 'h'},
+        {"version", no_argument, nullptr, 'V'},
+        {"test", required_argument, nullptr, 0},
+        nullptr
+    };
 
-  do {
-    int optIndex = -1;
-    std::unique_ptr<struct option> opt = nullptr;
-    auto result = getopt_long(argc, argv, "hV", long_options, &optIndex);
+    do {
+        int optIndex = -1;
+        std::unique_ptr<struct option> opt = nullptr;
+        auto result = getopt_long(argc, argv, "hV", long_options, &optIndex);
 
-    if (result == -1) {
-      break;
+        if (result == -1) {
+            break;
+        }
+
+        switch (result) {
+            case '?':
+                LOG(INFO) << "Unknown option given";
+                std::cout << "Not know\n";
+                break;
+
+            case 'h':
+                LOG(INFO) << "Help option given";
+                std::cout << "long h\n";
+                break;
+
+            case 'V':
+                LOG(INFO) << "Version option given";
+                std::cout << "long V\n";
+
+            case '0':
+                opt = std::make_unique<struct option>(long_options[optIndex]);
+                LOG(INFO) << "Option " << opt->name << " given";
+
+                if (opt->has_arg == required_argument) {
+                    LOG(INFO) << "Argument: " << optarg;
+                }
+
+                break;
+
+            default:
+                std::cout << "I shouldnt have been here!\n";
+                break;
+        }
+    } while (true);
+
+    LOG(INFO) << "Parsing options done";
+    std::optional<std::string> filename = {};
+    LOG(INFO) << "Parsing other arguments...";
+
+    while (optind < argc) {
+        if (filename.has_value()) {
+            LOG(ERROR) << "Only one filename can be given!";
+            throw std::invalid_argument("Only one filename can be given!\n");
+        }
+
+        LOG(INFO) << "Filename set to: " << argv[optind];
+        filename = std::string(argv[optind++]);
     }
 
-    switch (result) {
-    case '?':
-      LOG(INFO) << "Unknown option given";
-      std::cout << "Not know\n";
-      break;
-
-    case 'h':
-      LOG(INFO) << "Help option given";
-      std::cout << "long h\n";
-      break;
-
-    case 'V':
-      LOG(INFO) << "Version option given";
-      std::cout << "long V\n";
-
-    case '0':
-      opt = std::make_unique<struct option>(long_options[optIndex]);
-      LOG(INFO) << "Option " << opt->name << " given";
-
-      if (opt->has_arg == required_argument) {
-        LOG(INFO) << "Argument: " << optarg;
-      }
-      break;
-    default:
-        std::cout << "I shouldnt have been here!\n";
-      break;
-    }
-  } while (true);
-
-  LOG(INFO) << "Parsing options done";
-  std::optional<std::string> filename = {};
-  LOG(INFO) << "Parsing other arguments...";
-
-  while (optind < argc) {
-    if (filename.has_value()) {
-      LOG(ERROR) << "Only one filename can be given!";
-      throw std::invalid_argument("Only one filename can be given!\n");
-    }
-
-    LOG(INFO) << "Filename set to: " << argv[optind];
-    filename = std::string(argv[optind++]);
-  }
-
-  return filename;
+    return filename;
 }
 } // namespace utils
